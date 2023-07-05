e4eadb5fb4eee5f2c490881fa4ff97e5fe7548db
C. Wright <cwright43@live.com>
2023-07-04 18:20:58 -0600

    5. Token Transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,15 +10,40 @@ contract Token {
 	uint256 public totalSupply = 1000000 * (10**decimals);
 	
 
-	// Track balances
+	
 	mapping(address => uint256) public balanceOf;
-	// Send tokes
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+	event Transfer(
+		address indexed from, 
+		address indexed to, 
+		uint256 value
+	);
+
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply
+	) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
 
+	function transfer(address _to, uint256 _value) 
+	public 
+	returns (bool success) 
+{
+	//Deduct balance from sender
+	require(balanceOf[msg.sender] >= _value); // Evaluates to either TRUE or FALSE
+	require(_to != address(0));
+
+	balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
+	balanceOf[_to] = balanceOf[_to] + _value;
+
+	emit Transfer(msg.sender, _to, _value);
+
+	return true;
+	}
 }
